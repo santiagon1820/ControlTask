23cd737ccaeba6b71535dbdd95e47dcda326b244
santiagon1820 <230399363+santiagon1820@users.noreply.github.com>
2026-03-20 10:25:41 -0600

    feat: implement user authentication with login and logout functionality, including JWT, bcrypt, and Pydantic schemas for API responses.

--- schemas/Schemas.py
+++ schemas/Schemas.py
@@ -10,12 +10,13 @@ class Login200(BaseModel):
             "example":{
                 "message":"Login exitoso",
                 "token":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoxLCJlbWFpbCI6ImFkbWluQGV4YW1wbGUuY29tIiwidXNlcm5hbWUiOiJhZG1pbiIsInR5cGUiOiJhZG1pbiIsInNjb3BlIjoiZnVsbCIsImV4cCI6MTc0MjQyOTc4N30.XyZ1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef",
-                "type":"1"
+                "type":"1",
+                "expires_at":"2025-01-01 00:00:00"
             }
         }
     }
 
-class Login400(BaseModel):
+class Login401(BaseModel):
     error: str
     model_config = {
         "json_schema_extra":{
